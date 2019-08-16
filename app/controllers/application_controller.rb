c2a0431cabfe5b0e9b185b6389a1af48c7c04295
kv-2 <str_token@mail.ru>
2019-08-16 19:58:25 +0300

    add test with rspec and postman

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::API
+  require 'json_web_token'
   def not_found
     render json: { error: 'not_found' }
   end
--- app/controllers/signin_controller.rb
+++ app/controllers/signin_controller.rb
@@ -1,22 +1,25 @@
 class SigninController < ApplicationController
+  require 'json_web_token'
   before_action :authorize_request, except: :create
 
   # POST /signin
   def create
-    @user = User.find_by_email(params[:email])
+    user = User.find_by(email: params[:email].to_s)
     if user && user.authenticate(params[:password])
-      token = JsonWebToken.encode(user_id: @user.id)
+      token = JsonWebToken.encode(user_id: user.id)
       time = Time.now + 24.hours.to_i
       render json: { token: token, exp: time.strftime("%m-%d-%Y %H:%M"),
-                     user_email: @user.email }, status: :ok
+                     user_email: user.email }, status: :ok
     else
-      render json: { error: 'unauthorized' }, status: :unauthorized
+      render json: {error: 'Invalid username / password'}, status: :unauthorized
     end
   end
 
   private
 
-  def login_params
-    params.permit(:email, :password)
+  def user_params
+    params.permit(
+      :email, :password
+    )
   end
 end
