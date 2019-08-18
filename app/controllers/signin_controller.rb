2fcea99c757e1a5971c16b276c0cd899fc687939
kv-2 <str_token@mail.ru>
2019-08-18 18:24:21 +0300

    add actions reset and destroy

--- app/controllers/signin_controller.rb
+++ app/controllers/signin_controller.rb
@@ -1,13 +1,15 @@
 class SigninController < ApplicationController
   require 'json_web_token'
   before_action :authorize_request, except: :create
+  before_action :set_current_user_exp
 
   # POST /signin
   def create
     user = User.find_by(email: params[:email].to_s)
     if user && user.authenticate(params[:password])
       token = JsonWebToken.encode(user_id: user.id)
-      time = Time.now + 24.hours.to_i
+      session[:current_user_token] = token
+      time = session[:current_user_exp]
       render json: { token: token, exp: time.strftime("%m-%d-%Y %H:%M"),
                      user_email: user.email }, status: :ok
     else
@@ -15,7 +17,26 @@ class SigninController < ApplicationController
     end
   end
 
+  def reset
+    reset_session
+  end
+
+  def destroy
+    session.delete(:current_user_token)
+    @current_user = nil
+    render json: :ok
+  end
+
   private
+
+  def set_current_user_exp
+    if session[:current_user_exp] && session[:current_user_exp] < (Time.now - 12.hours.to_i)
+      time_exp=session[:current_user_exp]
+    else
+      time_exp = Time.now + 24.hours.to_i
+    end
+    session[:current_user_exp]=time_exp
+  end
 
   def user_params
     params.permit(
