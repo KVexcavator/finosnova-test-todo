1134b00f485066f2f92169c5f2de20026f6fecd1
kv-2 <str_token@mail.ru>
2019-08-18 18:25:49 +0300

    add test

--- spec/controllers/signin_controller_spec.rb
+++ spec/controllers/signin_controller_spec.rb
@@ -2,14 +2,15 @@ require 'rails_helper'
 
 RSpec.describe SigninController, type: :controller do
   describe 'POST #create' do
-    
-    let(:user_params) { { email: 'rspec@mail.com', password: 'rspec333' } }
-    # do it postman
-    # it 'returns http success' do
-    #   post :create, params: user_params
-    #   expect(response).to be_successful
-    #   expect(response_json.keys).to eq ['token']
-    # end
+    let(:password){'password'}
+    let(:user){ create(:user) }
+    let(:user_params) { { email: user.email, password: password } }
+
+     it 'returns http success' do
+       post :create, params: user_params
+       expect(response).to be_successful
+       expect(response_json.keys).to eq ["token", "exp", "user_email"]
+     end
 
     it 'returns unauthorized for invalid params' do
       post :create, params: { email: 'rspec@mail.com', password: 'incorrect' }
